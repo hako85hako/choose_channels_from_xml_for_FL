e53ee9b34d3b58b6afc528d5f8ebd26d2b27b874
sakai <sakai@f-logic.jp>
2022-03-18 18:03:36 +0900

    バグ調整

--- source/ch_shift.py
+++ source/ch_shift.py
@@ -6,6 +6,10 @@ from glob import glob
 import frame
 
 def main(dir_name,all_data,none_select_column,none_valiable_column,use_coefficient_flg):
+    #test_flg
+    test_flg = True
+
+
     #初期設定
     faile_ch_exchange = 'チャンネル置換中にエラーが発生しました'
     not_read_file = 'CSVファイルを読み込めません'
@@ -15,8 +19,10 @@ def main(dir_name,all_data,none_select_column,none_valiable_column,use_coefficie
     #data以下のファイル格納場所
     files = glob(f'{dir_name}/data/*')
     #各種値格納
-    old_file_offset = all_data[0][2] 
-    new_file_offset = all_data[0][3]
+    #old_file_offset = all_data[0][2] 
+    #new_file_offset = all_data[0][3]
+    old_file_offset = all_data[0][2] - 1
+    new_file_offset = all_data[0][3] - 1
     password = all_data[0][1]
 
     for file in files:
@@ -47,9 +53,9 @@ def main(dir_name,all_data,none_select_column,none_valiable_column,use_coefficie
                 #結果の入子
                 items = []
                 try:
-                    for i in range(7):#range(int(old_file_offset)-1):
-                        #print(i)
-                        if i == 6 :
+                    #for i in range(7):#range(int(old_file_offset)-1):
+                    for i in range(int(old_file_offset)+1):
+                        if i == int(old_file_offset) :
                             #offsetが同じ場合の場合
                             if old_file_offset == new_file_offset:
                                 item = row[i]
@@ -57,8 +63,6 @@ def main(dir_name,all_data,none_select_column,none_valiable_column,use_coefficie
                                 break
                             #offset8→7の場合
                             elif old_file_offset > new_file_offset:
-                                item = row[i]
-                                items += [item]
                                 break
                             #offset7→8の場合
                             elif old_file_offset < new_file_offset:
@@ -73,12 +77,31 @@ def main(dir_name,all_data,none_select_column,none_valiable_column,use_coefficie
                 except:
                     return frame.show_error(offset_write_error)
 
+
+                test_flg = True
+                test_count = 0
+                test_list = list()
                 for ch in all_data[1]:
                     #各種値格納
                     #係数に関しては、該当なければslop=1,intercept=0を格納
                     old_columnno = ch[0]
-                    old_slop = 1 if ch[1] == '--' else ch[1]
-                    old_intercept = 0 if ch[2] == '--' else ch[2]
+                    #old_slop = 1 if ch[1] == '--' else ch[1]
+                    #old_intercept = 0 if ch[2] == '--' else ch[2]
+                    if ch[6] == '--' :
+                        old_slop = 1
+                        old_intercept = 0
+                    else:
+                        
+                        try:
+                            old_slop = all_data[1][int(ch[6])-1][1]
+                        except:
+                            old_slop = 1
+                        
+                        try:
+                            old_intercept = all_data[1][int(ch[6])-1][2] 
+                        except:   
+                            old_intercept = 0
+
                     new_columnno = ch[3]
                     new_slop = 1 if ch[4] == '--' else ch[4]
                     new_intercept = 0 if ch[5] == '--' else ch[5]
@@ -86,7 +109,9 @@ def main(dir_name,all_data,none_select_column,none_valiable_column,use_coefficie
 
                     if new_columnno == '--':
                         break
+                    
 
+                    
                     try:
                         #チャンネルの指定があるかの判定
                         if type(shift_num) is int :
@@ -95,14 +120,22 @@ def main(dir_name,all_data,none_select_column,none_valiable_column,use_coefficie
                             try:
                                 shift_num = shift_num + old_file_offset
                                 item = float(row[shift_num])
+
                                 #係数有効flgの確認
                                 if use_coefficient_flg == "1":
                                     #係数の処理
                                     item = ( item / old_slop ) - old_intercept
+                                    #item = ( item / new_slop ) - new_intercept
                                     item = round(item,7)#DC対応のため、小数点以下7桁まで取得
                                 #整数であれば小数点以下を削除
                                 if(item.is_integer()):
                                     item = int(item)
+                                
+                                ####test
+                                if test_flg:
+                                    test_list += [shift_num,item,old_slop,old_intercept]
+                                    test_count += 1
+                                
                             except:
                                 #元の値がnullの場合はそのまま格納する
                                 if row[shift_num] == "null" or row[shift_num] == "Null" or row[shift_num] == "\(null\)":
@@ -114,6 +147,8 @@ def main(dir_name,all_data,none_select_column,none_valiable_column,use_coefficie
                     except:
                         item = none_valiable_column
                     items += [item]
+                if test_count > 10:
+                    test_flg = False
                 data += [items]
 
         try:
@@ -126,7 +161,8 @@ def main(dir_name,all_data,none_select_column,none_valiable_column,use_coefficie
             return frame.show_error(faile_write_file)
         finally:
             f.close()
-        
+    print(test_list)#test
+    #print(all_data)#test
     return frame.show_info(correct_replace)
 
 
